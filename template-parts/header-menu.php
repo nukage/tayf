15486c48fb074e2c42bd90522b14fa868b5fecba
Tom Myruski <thomas.myruski@gmail.com>
2017-04-25 16:32:35 -0400

    First Commit

---
+++ template-parts/header-menu.php
@@ -0,0 +1,25 @@
+ <div class="collapse navbar-collapse navbar-right navbar-main-collapse">
+      <ul class="nav navbar-nav">
+        <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+      
+        <li> <a class="page-scroll" href="<?php echo get_site_url() ; ?>/">Home</a> </li>
+        <li> <a class="page-scroll" href="#">About</a> </li>
+        <li> <a class="page-scroll" href="<?php echo get_site_url() ; ?>/#artists">Artists</a> </li>
+        <li> <a class="page-scroll" href="<?php echo get_site_url() ; ?>/press-release">Releases</a> </li>
+       </li><li>
+       <form role="search" method="get" id="searchform" class="searchform navbar-form" action="<?php echo get_site_url() ; ?>"> 
+<!-- <label class="screen-reader-text" for="s">Search for:</label>  -->
+ <div class="input-group">
+<input type="text" value="" name="s" id="s" placeholder="Search Clients" class="form-control search-custom-form"  /> 
+    <div class="input-group-btn">
+                <button class="btn btn-default search-custom-button" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+            </div>
+</div>
+</form>
+
+
+    
+        </li>
+
+      </ul>
+    </div>
