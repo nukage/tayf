df9d84a85c2d004dbad6fb8dc09291fdf9a898b9
Tom Myruski <thomas.myruski@gmail.com>
2017-05-05 16:53:08 -0400

    search clients > artists

--- template-parts/header-menu.php
+++ template-parts/header-menu.php
@@ -11,7 +11,7 @@
        <form role="search" method="get" id="searchform" class="searchform navbar-form" action="<?php echo get_site_url() ; ?>"> 
 <!-- <label class="screen-reader-text" for="s">Search for:</label>  -->
  <div class="input-group">
-<input type="text" value="" name="s" id="s" placeholder="Search Clients" class="form-control search-custom-form"  /> 
+<input type="text" value="" name="s" id="s" placeholder="Search Artists" class="form-control search-custom-form"  /> 
     <div class="input-group-btn">
                 <button class="btn btn-default search-custom-button" type="submit"><i class="glyphicon glyphicon-search"></i></button>
             </div>
