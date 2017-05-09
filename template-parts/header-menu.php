81f282a35536de3ef1768a9885c6c5408e11c8e4
Tom Myruski <thomas.myruski@gmail.com>
2017-05-09 14:16:38 -0400

    Footer stuff, email icon on staff page is better

--- template-parts/header-menu.php
+++ template-parts/header-menu.php
@@ -12,6 +12,7 @@
 <!-- <label class="screen-reader-text" for="s">Search for:</label>  -->
  <div class="input-group">
 <input type="text" value="" name="s" id="s" placeholder="Search Artists" class="form-control search-custom-form"  /> 
+<input type="hidden" name="post_type" value="artist" /> 
     <div class="input-group-btn">
                 <button class="btn btn-default search-custom-button" type="submit"><i class="glyphicon glyphicon-search"></i></button>
             </div>
