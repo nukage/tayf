8c74b9124a56f62f50b8b0c6cad19b6021c72b69
Tom Myruski <thomas.myruski@gmail.com>
2017-05-17 15:44:25 -0400

    Various changes
    Styles for captions
    Info in footer added (telephone instead of address)

--- template-parts/footer-widgets.php
+++ template-parts/footer-widgets.php
@@ -1,11 +1,10 @@
     <div class="col-sm-8 footer-contacts wow fadeInLeft animated bcard" data-wow-delay="1s">
 
     <img src="<?php echo get_stylesheet_directory_uri()?>/img/layout/TAYF_FOOTOERLOGO.png"  height="auto" alt="Tell All Your Friends PR">
-<p>MANHATTAN OFFICE:<br>
-        <a href="https://www.google.com/maps?q=59+Franklin+St,+New+York,+New+York&um=1&ie=UTF-8&sa=X&ved=0ahUKEwjVv5ynztnTAhVl5IMKHeuBAksQ_AUICigB" target="_blank">59 Franklin St, New York, NY 10013</a></p>
+   <p>MANHATTAN OFFICE:<br>
+        <a href="tel:(212)226-3792" target="_blank">(212)226-3792</a></p>
         <p>BROOKLYN OFFICE:<br>
-        <a href="https://www.google.com/maps/place/147+Front+St,+Brooklyn,+NY+11201/@40.7027537,-73.9892931,17z/data=!3m1!4b1!4m5!3m4!1s0x89c25a33960c1381:0x60b33275417912bd!8m2!3d40.7027497!4d-73.9870991" target="_blank">147 Front St, Brooklyn, NY 11201</a></p>
-    
+        <a href="tel:(212)226-3792" target="_blank">(555)224-2525</a></p>  
         </div>
 
     
