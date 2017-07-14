64a3392a9c99a6799d86d4654a9fc26e48f79fb1
Tom Myruski <thomas.myruski@gmail.com>
2017-07-14 15:48:19 -0400

    added spotify

--- template-parts/home-sidebar1.php
+++ template-parts/home-sidebar1.php
@@ -1,4 +1,11 @@
-  <div class="col-md-6   home-events wow fadeInUp" data-wow-delay=".2s">      <div class="underline"> 
+    <div class="col-md-6   home-events wow fadeInUp" data-wow-delay=".2s">  <div class="underline"> 
+                                <h4>SPOTIFY</h4> 
+               
+                              </div>
+<iframe src="https://embed.spotify.com/?uri=spotify%3Auser%3Atellallyourfriendspr%3Aplaylist%3A5vWsoxn15uMNUbjZrNCYJ5&amp;theme=white" style="border:0" width="100%" height="700"></iframe>
+</div>
+                           
+  <div class="col-md-5 col-md-push-1   home-events wow fadeInUp" data-wow-delay=".2s">      <div class="underline"> 
                                 <h4>INSTAGRAM</h4> 
                                 <a href="http://www.instagram.com/tayfpr" target="_blank" class="btn-view-all pull-right text-right">View All</a> 
                               </div>
